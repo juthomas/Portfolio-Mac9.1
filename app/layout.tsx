dde58259f089f4ceed4a96f02c5551b3fd58f552
juthomas <juthomas@student.42.fr>
2023-09-21 17:31:12 +0200

    [ADD] Navbar in progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import './fonts.css';
 import React from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: any }) {
       <head>
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/favicon.svg" />
+        <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
---
+++ components/Header/Header.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import Image from 'next/image';
+import { Menu, Group, Center, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+// import { MantineLogo } from '@mantine/ds';
+import classes from './Header.module.css';
+import juthomasLogo from '@/assets/juthomas_logo.svg';
+
+const links = [
+  {
+    link: '#1',
+    label: 'File',
+    links: [
+      { link: '/docs', label: 'Documentation' },
+      { link: '/resources', label: 'Resources' },
+      { link: '/community', label: 'Community' },
+      { link: '/blog', label: 'Blog' },
+    ],
+  },
+  {
+    link: '#2',
+    label: 'Edit',
+    links: [
+      { link: '/faq', label: 'FAQ' },
+      { link: '/demo', label: 'Book a demo' },
+      { link: '/forums', label: 'Forums' },
+    ],
+  },
+];
+
+export function Header() {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const items = links.map((link) => {
+    const menuItems = link.links?.map((item) => (
+      <Menu.Item key={item.link}>{item.label}</Menu.Item>
+    ));
+
+    if (menuItems) {
+      return (
+        <Menu key={link.label} trigger="click" transitionProps={{ exitDuration: 0 }} withinPortal>
+          <Menu.Target>
+            <a
+              href={link.link}
+              className={classes.link}
+              onClick={(event) => event.preventDefault()}
+            >
+              <Center>
+                <span className={classes.linkLabel}>{link.label}</span>
+                {/* <IconChevronDown size="0.9rem" stroke={1.5} /> */}
+              </Center>
+            </a>
+          </Menu.Target>
+          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+        </Menu>
+      );
+    }
+
+    return (
+      <a
+        key={link.label}
+        href={link.link}
+        className={classes.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </a>
+    );
+  });
+
+  return (
+    <header className={classes.header}>
+      {/* <Container size="md"> */}
+      <div className={classes.inner}>
+        {/* <MantineLogo size={28} /> */}
+        <Group style={{ height: '100%', padding: 4 }} gap={15} visibleFrom="sm">
+          {/* <Image
+            src={juthomasLogo}
+            alt="logo"
+            width={0}
+            height={0}
+            style={{ width: 'auto', height: '100%' }}
+          /> */}
+          <Menu trigger="click" transitionProps={{ exitDuration: 0 }} withinPortal>
+            <Menu.Target>
+              <Image
+                src={juthomasLogo}
+                alt="logo"
+                width={0}
+                height={0}
+                style={{ width: 'auto', height: '100%' }}
+              />
+            </Menu.Target>
+            <Menu.Dropdown>À propos de cette page</Menu.Dropdown>
+          </Menu>
+
+          {items}
+        </Group>
+        <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+      </div>
+      {/* </Container> */}
+    </header>
+  );
+}
