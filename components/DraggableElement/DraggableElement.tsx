b4ddecb80d06403208469df000d0ba7b6fb1363f
juthomas <juthomas@student.42.fr>
2023-09-22 20:36:44 +0200

    [ADD] Minimize button working

--- components/DraggableElement/DraggableElement.tsx
+++ components/DraggableElement/DraggableElement.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import { useDraggable } from '@dnd-kit/core';
 import { Box, Group } from '@mantine/core';
+import { useState } from 'react';
 import fileIcon from '@/assets/file_icon.svg';
 import classes from './DraggableElement.module.css';
 
@@ -24,51 +25,63 @@ export function DraggableElement({
     disabled: false,
   });
 
+  const [minimized, setMinimized] = useState(false);
+
   return (
     <Box
       className={classes.windowOuter}
       style={{
-        height: 400,
+        height: minimized ? undefined : 400,
+        paddingBottom: minimized ? 0 : undefined,
         width: 600,
         top,
         left,
         transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
       }}
     >
-      <Box className={classes.dragHandle} ref={setNodeRef} {...listeners} {...attributes}>
+      <Box className={classes.windowBar}>
         <Box className={classes.button} />
-        <Box className={classes.stripes}>
-          <Box className={classes.stripe} />
-          <Box className={classes.stripe} />
-          <Box className={classes.stripe} />
-          <Box className={classes.stripe} />
-        </Box>
-        <Group gap={5} px={10}>
-          <Image
-            src={windowIcon}
-            alt="logo"
-            width={0}
-            height={0}
-            style={{ width: 'auto', height: '70%' }}
-          />
-          {windowName}
-        </Group>
-        <Box className={classes.stripes}>
-          <Box className={classes.stripe} />
-          <Box className={classes.stripe} />
-          <Box className={classes.stripe} />
-          <Box className={classes.stripe} />
+        <Box className={classes.dragHandle} ref={setNodeRef} {...listeners} {...attributes}>
+          <Box className={classes.stripes}>
+            <Box className={classes.stripe} />
+            <Box className={classes.stripe} />
+            <Box className={classes.stripe} />
+            <Box className={classes.stripe} />
+          </Box>
+          <Group gap={5} px={10}>
+            <Image
+              src={windowIcon}
+              alt="logo"
+              width={0}
+              height={0}
+              style={{ width: 'auto', height: '70%' }}
+            />
+            {windowName}
+          </Group>
+          <Box className={classes.stripes}>
+            <Box className={classes.stripe} />
+            <Box className={classes.stripe} />
+            <Box className={classes.stripe} />
+            <Box className={classes.stripe} />
+          </Box>
         </Box>
         <Group gap={3}>
           <Box className={classes.button}>
             <Box className={classes.maximizeButtonBox} />
           </Box>
-          <Box style={{ display: 'flex', alignItems: 'center' }} className={classes.button}>
+          <Box
+            style={{ display: 'flex', alignItems: 'center' }}
+            className={classes.button}
+            onClick={() => {
+              setMinimized((value) => !value);
+            }}
+          >
             <Box className={classes.minimizeButtonBox} />
           </Box>
         </Group>
       </Box>
-      <Box className={classes.windowInner}>{children}</Box>
+
+      {!minimized && <Box className={classes.windowInner}>{children}</Box>}
     </Box>
   );
 }
