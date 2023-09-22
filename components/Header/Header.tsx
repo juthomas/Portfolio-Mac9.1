ba802c5024c878c253e7148c114f41b2b9550dfc
juthomas <juthomas@student.42.fr>
2023-09-22 11:56:32 +0200

    [ADD] Time in menu bar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import Image from 'next/image';
-import { Menu, Group, Center, Burger } from '@mantine/core';
+import { Menu, Group, Burger, Flex } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { useEffect, useState } from 'react';
 // import { MantineLogo } from '@mantine/ds';
 import classes from './Header.module.css';
 import juthomasLogo from '@/assets/juthomas_logo.svg';
+import fileIcon from '@/assets/file_icon.svg';
+import frenchFlag from '@/assets/french_flag.svg';
 
 const links = [
   {
@@ -41,22 +44,26 @@ export function Header() {
       return (
         <Menu
           // className={classes.linkMenu}
+          position="bottom-start"
           key={link.label}
           trigger="click"
           transitionProps={{ exitDuration: 0 }}
           withinPortal
         >
           <Menu.Target>
-            <a
-              href={link.link}
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
-            >
-              <Center>
+            <Flex align="center" className={classes.link}>
+              <a
+                href={link.link}
+                onClick={(event) => event.preventDefault()}
+                style={{
+                  textDecoration: 'none',
+                  color: 'light-dark(var(--mantine-color-dark-9), var(--mantine-color-dark-0))',
+                }}
+              >
                 <span className={classes.linkLabel}>{link.label}</span>
                 {/* <IconChevronDown size="0.9rem" stroke={1.5} /> */}
-              </Center>
-            </a>
+              </a>
+            </Flex>
           </Menu.Target>
           <Menu.Dropdown className={classes.linkMenu}>{menuItems}</Menu.Dropdown>
         </Menu>
@@ -75,37 +82,100 @@ export function Header() {
     );
   });
 
+  const pad = (val: number): string => (val < 10 ? `0${val}` : `${val}`);
+
+  const [time, setTime] = useState('12:00 PM');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let hours = new Date().getHours();
+      let minutes: string | number = new Date().getMinutes();
+      const ampm = hours >= 12 ? 'PM' : 'AM';
+      hours %= 12;
+      hours = hours || 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? `0${minutes}` : minutes;
+      setTime(`${hours}:${minutes} ${ampm}`);
+
+      document.title = `${pad(new Date().getHours())}:${pad(new Date().getMinutes())}:${pad(
+        new Date().getSeconds()
+      )}`;
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <header className={classes.header}>
       {/* <Container size="md"> */}
       <div className={classes.inner}>
         {/* <MantineLogo size={28} /> */}
-        <Group style={{ height: '100%', padding: 3 }} gap={15} visibleFrom="sm">
-          {/* <Image
+        <Group style={{ height: '100%', width: '100%' }} justify="space-between">
+          <Group style={{ height: '100%' }} gap={15} visibleFrom="sm">
+            {/* <Image
             src={juthomasLogo}
             alt="logo"
             width={0}
             height={0}
             style={{ width: 'auto', height: '100%' }}
           /> */}
-          <Menu trigger="click" transitionProps={{ exitDuration: 0 }} withinPortal>
-            <Menu.Target>
-              <div className={classes.link}>
-                <Image
-                  src={juthomasLogo}
-                  alt="logo"
-                  width={0}
-                  height={0}
-                  style={{ width: 'auto', height: '100%' }}
-                />
-              </div>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item>À propos de cette page</Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
+            <Menu
+              position="bottom-start"
+              trigger="click"
+              transitionProps={{ exitDuration: 0 }}
+              withinPortal
+            >
+              <Menu.Target>
+                <Flex align="center" className={classes.link}>
+                  <Image
+                    src={juthomasLogo}
+                    alt="logo"
+                    width={0}
+                    height={0}
+                    style={{ width: 'auto', height: '100%' }}
+                  />
+                </Flex>
+              </Menu.Target>
+              <Menu.Dropdown className={classes.linkMenu}>
+                <Menu.Item>À propos de cette page</Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
 
-          {items}
+            {items}
+          </Group>
+          <Group className={classes.menuBar} style={{ height: '100%' }}>
+            <Group gap={4} style={{ height: '100%' }}>
+              <Image
+                src={fileIcon}
+                alt="logo"
+                width={0}
+                height={0}
+                style={{ width: 'auto', height: '100%' }}
+              />
+              Portfolio
+            </Group>
+            <Menu
+              position="bottom-start"
+              trigger="click"
+              transitionProps={{ exitDuration: 0 }}
+              withinPortal
+            >
+              <Menu.Target>
+                <Flex align="center" className={classes.link}>
+                  <Image
+                    src={frenchFlag}
+                    alt="logo"
+                    width={0}
+                    height={0}
+                    style={{ width: 'auto', height: '80%' }}
+                  />
+                </Flex>
+              </Menu.Target>
+
+              <Menu.Dropdown className={classes.linkMenu}>
+                <Menu.Item>À propos de cette page</Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+            {time}
+          </Group>
         </Group>
         <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
       </div>
