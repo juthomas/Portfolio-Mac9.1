76759d0d852a8faf125d4967dcc5f7c614df50d2
juthomas <juthomas@student.42.fr>
2023-09-26 14:15:42 +0200

    [ADD] Overflow for Shortcuts

--- components/Desktop/Desktop.tsx
+++ components/Desktop/Desktop.tsx
@@ -5,12 +5,19 @@ import { DndContext, useSensors, useSensor, PointerSensor } from '@dnd-kit/core'
 import DraggableShortcut from '../DraggableShortcut/DraggableShortcut';
 import onefortreeIcon from '@/assets/onefortree.png';
 import iotaIcon from '@/assets/iota.png';
+import useWindowDimensions from '@/hooks/useWindowDImensions';
 
 export default function Desktop({ openWindow }: { openWindow: (windowId: string) => void }) {
   const [shortcutsPositions, setShortcutPositions] = useState([
     { id: '1', position: { x: 0, y: 50 }, windowId: '1', text: 'One for tree' },
     { id: '2', position: { x: 0, y: 30 }, windowId: '2', text: 'Projet Iota ' },
-    { id: '3', position: { x: 0, y: 30 }, link: 'http://projet-iota.fr', text: 'Projet Iota', icon: iotaIcon },
+    {
+      id: '3',
+      position: { x: 0, y: 30 },
+      link: 'http://projet-iota.fr',
+      text: 'Projet Iota',
+      icon: iotaIcon,
+    },
     {
       id: '4',
       position: { x: 0, y: 30 },
@@ -27,6 +34,7 @@ export default function Desktop({ openWindow }: { openWindow: (windowId: string)
       },
     })
   );
+  const { height: windowHeight, width: windowWidth } = useWindowDimensions();
 
   return (
     <DndContext
@@ -42,14 +50,23 @@ export default function Desktop({ openWindow }: { openWindow: (windowId: string)
           }
           shortcutsPositions.push(elementAdeplacer);
         }
-
         setShortcutPositions([...shortcutsPositions]);
       }}
       onDragEnd={({ delta, active }) => {
         const shortcutIndex = shortcutsPositions.findIndex((x) => x.id === active.id);
         shortcutsPositions[shortcutIndex].position = {
-          x: shortcutsPositions[shortcutIndex].position.x + delta.x,
-          y: shortcutsPositions[shortcutIndex].position.y + delta.y,
+          x:
+            shortcutsPositions[shortcutIndex].position.x + delta.x + 140 > windowWidth
+              ? windowWidth - 140
+              : shortcutsPositions[shortcutIndex].position.x + delta.x < 0
+              ? 0
+              : shortcutsPositions[shortcutIndex].position.x + delta.x,
+          y:
+            shortcutsPositions[shortcutIndex].position.y + delta.y + 120 > windowHeight
+              ? windowHeight - 120
+              : shortcutsPositions[shortcutIndex].position.y + delta.y < 35
+              ? 35
+              : shortcutsPositions[shortcutIndex].position.y + delta.y,
         };
         setShortcutPositions([...shortcutsPositions]);
       }}
