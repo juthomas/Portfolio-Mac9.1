e455dd01a435ac8f3cc24a92131ec0a91d137872
juthomas <juthomas@student.42.fr>
2023-10-19 23:47:19 +0200

    [CLEAN] Carousel

--- Windows/ProjectsWindow/ProjectsWindow.tsx
+++ Windows/ProjectsWindow/ProjectsWindow.tsx
@@ -1,20 +1,19 @@
-import Image from 'next/image';
-import { EmblaOptionsType } from 'embla-carousel-react';
 import { Box, Flex, List, Space, Text, Title } from '@mantine/core';
-import DraggableShortcut from '@/components/DraggableShortcut/DraggableShortcut';
-import juthomasPicture from '@/assets/juthomas-picture.png';
-import classes from '@/styles/theme.module.css';
-import { Carousel } from '@/components/Carousel/Carousel';
 
-const OPTIONS: EmblaOptionsType = { loop: true };
-const SLIDE_COUNT = 5;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+import { Carousel } from '@/components/Carousel/Carousel';
+import mdma from '@/assets/carousels/mdma.webp';
+import iota from '@/assets/carousels/holy.webp';
 
 export default function ProjectsWindow(): JSX.Element {
+  const slides = [
+    { image: mdma, alt: 'mdma image' },
+    { image: iota, alt: 'iota image' },
+  ];
+
   return (
     <Flex direction="column" align="center" h="100%">
       <Flex direction="column" align="center" h="100%" w="100%" pt="xl" maw="650px">
-        <Carousel slides={SLIDES} options={OPTIONS} />
+        <Carousel slides={slides} options={{ loop: true }} />
       </Flex>
     </Flex>
   );
--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import React, { useCallback } from 'react';
 import useEmblaCarousel, { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
-import { Box, Flex } from '@mantine/core';
-import { PrevButton, NextButton, usePrevNextButtons } from './CarouselButtons';
+import { Box, Button, Flex } from '@mantine/core';
+import { usePrevNextButtons } from './CarouselButtons';
 // import imageByIndex from './imageByIndex';
 import mdma from '@/assets/carousels/mdma.webp';
 import iota from '@/assets/carousels/holy.webp';
@@ -14,7 +14,7 @@ import emblaClasses from './embla.module.css';
 import themeClasses from '@/styles/theme.module.css';
 
 type PropType = {
-  slides: number[];
+  slides: { image?: StaticImageData; alt?: string }[];
   options?: EmblaOptionsType;
 };
 
@@ -32,13 +32,14 @@ export const Carousel: React.FC<PropType> = (props) => {
     usePrevNextButtons(emblaApi, onButtonClick);
 
   return (
-    <Flex gap="xs">
-      <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+    <Flex gap="xs" align="center">
+      <Button onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+
       <Box className={themeClasses.retroBox}>
         <div className={emblaClasses.embla}>
           <div className={emblaClasses.embla__viewport} ref={emblaRef}>
             <div className={emblaClasses.embla__container}>
-              {slides.map((index) => (
+              {slides.map((slide, index) => (
                 <div className={emblaClasses.embla__slide} key={index}>
                   <div className={emblaClasses.embla__slide__number}>
                     <span>{index + 1}</span>
@@ -54,7 +55,7 @@ export const Carousel: React.FC<PropType> = (props) => {
           </div>
         </div>
       </Box>
-      <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+      <Button onClick={onNextButtonClick} disabled={nextBtnDisabled} />
     </Flex>
   );
 };
