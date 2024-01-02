104b580a0773e51b094f25fa96ab6433f34c2706
juthomas <juthomas@student.42.fr>
2024-01-02 20:12:27 +0100

    [ADD] Not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+import { Box, Stack, Text } from '@mantine/core';
+import Image from 'next/image';
+
+export default function NotFound() {
+  return (
+    <Box
+      style={{
+        height: '100%',
+        width: '100%',
+        position: 'relative',
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Stack align="center" gap={3}>
+        <Image
+          alt="puzzle_piece"
+          src="/bootingFolder.png"
+          height={64}
+          width={64}
+          style={{
+            zIndex: 40,
+            objectFit: 'contain',
+            imageRendering: 'pixelated',
+          }}
+        />
+        <Text px={10} style={{ backgroundColor: 'white' }}>Not Found</Text>
+      </Stack>
+    </Box>
+  );
+}
