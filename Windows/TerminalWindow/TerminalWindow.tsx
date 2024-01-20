038b3f0a589b05c6a584cfdba5157f3ad93c54a3
juthomas <juthomas@student.42.fr>
2024-01-20 19:15:57 +0100

    [ADD] Clear command

--- Windows/TerminalWindow/TerminalWindow.tsx
+++ Windows/TerminalWindow/TerminalWindow.tsx
@@ -25,6 +25,10 @@ export default function TerminalWindow(): JSX.Element {
 
   const commands: commandsType = {
     pwd: () => formatText(currentDirectory),
+    clear: () => {
+      setOldPrompts([]);
+      return <></>;
+    },
   };
 
   //   const;
