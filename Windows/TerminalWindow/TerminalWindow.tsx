5fba8f23baa6b2785514457b8b3acaaebda1f268
juthomas <juthomas@student.42.fr>
2024-01-20 19:13:34 +0100

    [UDPT] Terminal

--- Windows/TerminalWindow/TerminalWindow.tsx
+++ Windows/TerminalWindow/TerminalWindow.tsx
@@ -1,13 +1,33 @@
 import { useRef, useState } from 'react';
-import { Flex, Stack } from '@mantine/core';
+import { Box, Flex, Stack, Text } from '@mantine/core';
 import classes from './TerminalWindow.module.css';
 
+type commandsType = {
+  [key: string]: () => JSX.Element;
+};
+
 export default function TerminalWindow(): JSX.Element {
-  const [oldPrompts, setOldPrompts] = useState(['Hello World!']);
+  const [oldPrompts, setOldPrompts] = useState([{ prompt: 'Hello World!', answer: <></> }]);
 
   const [prompt, setPrompt] = useState('');
 
   const ref = useRef<HTMLInputElement | null>(null);
+
+  const [currentDirectory, setCurrentDirectory] = useState('/home/juthomas');
+
+  function formatText(value: string) {
+    return (
+      <Text style={{ lineHeight: '0.95' }} className={`${classes.text} ${classes.white} `}>
+        {value}
+      </Text>
+    );
+  }
+
+  const commands: commandsType = {
+    pwd: () => formatText(currentDirectory),
+  };
+
+  //   const;
 
   return (
     <Stack
@@ -21,32 +41,41 @@ export default function TerminalWindow(): JSX.Element {
       gap={0}
     >
       {oldPrompts.map((elem) => (
-        <Flex h="20px">
-          <span className={`${classes.text} ${classes.prompt} ${classes.green}`}>➜&nbsp;</span>
-          <span className={`${classes.text} ${classes.prompt} ${classes.blue}`}>~&nbsp;</span>
-          <span className={`${classes.text} ${classes.white} `}>{elem}</span>
-          {/* <input
-          className={`${classes.input} ${classes.text} ${classes.white} `}
-          style={{ width: '100%', maxWidth: '100%', flex: 1 }}
-        ></input> */}
-        </Flex>
+        <Box mb={1}>
+          <p
+            style={{ margin: 0, lineHeight: 0.95, wordBreak: 'break-all', whiteSpace: 'pre-wrap' }}
+          >
+            <span className={`${classes.text} ${classes.prompt} ${classes.green}`}>➜&nbsp;</span>
+            <span className={`${classes.text} ${classes.prompt} ${classes.blue}`}>~&nbsp;</span>
+            <span className={`${classes.text} ${classes.white} `}>{elem.prompt}</span>
+          </p>
+          {elem.answer}
+        </Box>
       ))}
-      <Flex>
+      <Flex style={{ marginTop: -5 }}>
         <span className={`${classes.text} ${classes.prompt} ${classes.green}`}>➜&nbsp;</span>
         <span className={`${classes.text} ${classes.prompt} ${classes.blue}`}>~&nbsp;</span>
         <input
+          spellCheck="false"
           data-autofocus
           ref={ref}
           onKeyDown={(e) => {
             if (e.key === 'Enter') {
-              setOldPrompts((old) => [...old, prompt]);
+              setOldPrompts((old) => [
+                ...old,
+                {
+                  prompt,
+                  answer: commands[prompt]
+                    ? commands[prompt]()
+                    : formatText(`zsh: command not found: ${prompt}`),
+                },
+              ]);
               setPrompt('');
             }
           }}
           onChange={(event) => setPrompt(event.target.value)}
           value={prompt}
-          className={`${classes.input} ${classes.text} ${classes.white} `}
-          style={{ width: '100%', maxWidth: '100%', flex: 1 }}
+          className={`${classes.input} ${classes.text} ${classes.white}`}
         />
       </Flex>
     </Stack>
