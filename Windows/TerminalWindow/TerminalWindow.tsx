c54a40c540692ca2e9e20df3dd962ad5aff75969
juthomas <juthomas@student.42.fr>
2024-01-21 14:32:40 +0100

    [UDPT] Open command

--- Windows/TerminalWindow/TerminalWindow.tsx
+++ Windows/TerminalWindow/TerminalWindow.tsx
@@ -8,61 +8,62 @@ type commandsType = {
 };
 
 type fileSystemType = {
-  [key: string]: fileSystemType | string;
-};
-
-const fileSystem: fileSystemType = {
-  Applications: {},
-  Library: {},
-  System: {},
-  Users: {
-    juthomas: {
-      //   'README.md': 'onefortree',
-      Applications: {
-        Onefortree: 'onefortree',
-        Portfolio: 'main',
-        Profile: 'profile',
-        Contact: 'contact',
-        Projects: 'projects',
-      },
-      Desktop: {},
-      Documents: {},
-      Downloads: {},
-      Library: {},
-      Movies: {},
-      Music: {},
-      Pictures: {},
-      Public: {},
-    },
-    guest: {
-      Applications: {},
-      Desktop: {},
-      Documents: {},
-      Downloads: {},
-      Library: {},
-      Movies: {},
-      Music: {},
-      Pictures: {},
-      Public: {},
-    },
-  },
-  Volumes: {},
-  bin: {},
-  cores: {},
-  dev: {},
-  etc: {},
-  home: {},
-  opt: {},
-  private: {},
-  sbin: {},
-  tmp: {},
-  usr: {},
-  var: {},
+  [key: string]: fileSystemType | (() => void) | string;
 };
 
 export default function TerminalWindow(): JSX.Element {
   const windowContext = useContext(WindowManagerContext);
 
+  const fileSystem: fileSystemType = {
+    Applications: {},
+    Library: {},
+    System: {},
+    Users: {
+      juthomas: {
+        //   'README.md': 'onefortree',
+        Applications: {
+          Onefortree: () => {
+            windowContext?.OpenWindow && windowContext?.OpenWindow('onefortree');
+          },
+          Portfolio: 'main',
+          Profile: 'profile',
+          Contact: 'contact',
+          Projects: 'projects',
+        },
+        Desktop: {},
+        Documents: {},
+        Downloads: {},
+        Library: {},
+        Movies: {},
+        Music: {},
+        Pictures: {},
+        Public: {},
+      },
+      guest: {
+        Applications: {},
+        Desktop: {},
+        Documents: {},
+        Downloads: {},
+        Library: {},
+        Movies: {},
+        Music: {},
+        Pictures: {},
+        Public: {},
+      },
+    },
+    Volumes: {},
+    bin: {},
+    cores: {},
+    dev: {},
+    etc: {},
+    home: {},
+    opt: {},
+    private: {},
+    sbin: {},
+    tmp: {},
+    usr: {},
+    var: {},
+  };
   const [currentDirectory, setCurrentDirectory] = useState('/Users/juthomas');
 
   function listDirectory(param: string) {
@@ -111,8 +112,6 @@ export default function TerminalWindow(): JSX.Element {
   }
 
   function openCommand(param: string) {
-    let directoryToList = currentDirectory;
-
     if (param) {
       const newPath =
         param[0] === '/'
@@ -124,13 +123,18 @@ export default function TerminalWindow(): JSX.Element {
       let currentPath = fileSystem;
       const exists = pathParts.every((part) => {
         if (currentPath[part]) {
-          if (typeof currentPath[part] === 'string') {
-            const windowId = currentPath[part];
-            typeof windowId === 'string' &&
-              windowContext?.OpenWindow &&
-              windowContext?.OpenWindow(windowId);
+          if (typeof currentPath[part] === 'function') {
+            const appFunction = currentPath[part];
+            typeof appFunction === 'function' && appFunction();
             return false;
           }
+          // 	if (typeof currentPath[part] === 'string') {
+          //     const windowId = currentPath[part];
+          //     typeof windowId === 'string' &&
+          //       windowContext?.OpenWindow &&
+          //       windowContext?.OpenWindow(windowId);
+          //     return false;
+          //   }
           if (typeof currentPath[part] === 'object') {
             currentPath = currentPath[part] as fileSystemType;
             return true;
@@ -142,29 +146,9 @@ export default function TerminalWindow(): JSX.Element {
       if (exists) {
         return `open : ${param}: Cant open file`;
       }
-    } else return 'Usage: open [filename]';
-    // const pathParts = directoryToList.split('/').filter((part) => part.length > 0);
-
-    // let currentPath = fileSystem;
-    // const pathExists = pathParts.every((part) => {
-    //   if (currentPath[part] !== null) {
-    //     if (typeof currentPath[part] === 'string') {
-    //       console.log('content :', currentPath[part]);
-    //     }
-    //     if (typeof currentPath[part] === 'object') {
-    //       currentPath = currentPath[part] as fileSystemType;
-    //       return true; // Continue l'itération
-    //     }
-    //   }
-    //   return false; // Arrête l'itération
-    // });
-
-    // if (!pathExists) {
-    //   return `open 2 : ${directoryToList}: No such file or directory`;
-    // }
-
-    // Récupérer les clés du répertoire courant pour les lister
-    // return Object.keys(currentPath).join('\n');
+      return '';
+    }
+    return 'Usage: open [filename]';
   }
 
   function changeDirectory(param: string) {
